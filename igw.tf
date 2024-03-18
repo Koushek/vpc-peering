a2bb3fb53f7732b45a8f8f5c8aca1635dd9e37ec
Koushek <145333384+Koushek@users.noreply.github.com>
2024-03-18 12:19:58 +0530

    Add files via upload

---
+++ igw.tf
@@ -0,0 +1,13 @@
+# internet_gateway.tf
+
+# Create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc1.id
+}
+
+# Associate internet gateway with VPC route table
+resource "aws_route" "route_to_internet" {
+  route_table_id            = aws_route_table.vpc1_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.gw.id
+}
